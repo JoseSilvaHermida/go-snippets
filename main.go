31a0c8e72c30070558db769a36f15b23bc20a9b5
Jose <jose.silva.h@gmail.com>
2021-05-21 22:51:57 +0200

    Module creation attempt

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/josesilvahermida/go-snippets/jshmodule"
 )
 
 func main() {
 	fmt.Println("Hi there!")
 	fmt.Println("General Kenobi")
+
+	jshmodule.Hello("Jose")
 }
